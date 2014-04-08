7984c87cf039da65dc75fed63151751fa42ca59e
Grant McGovern <grant@cobrain.com>
2014-04-08 12:10:51 -0400

    Added htmlWrite() and function to check Study Room Availability

--- Main.py
+++ Main.py
@@ -4,11 +4,13 @@
 import re 
 import datetime 
 import sys 
+import os # allows us to interact with paths and open the file in a relative location 
 import urllib
 import urllib2
 import socket
 import struct 
 import fcntl 
+import time
 from bs4 import BeautifulSoup
 from selenium import webdriver 
 from selenium.webdriver.common.keys import Keys 
@@ -16,20 +18,28 @@ from selenium.webdriver.common.keys import Keys
 
 def main():
 
+	date = getDate() # must make a call to get date and pass into URL to update with day 
 	############################################
+	url = "http://zsr.wfu.edu/studyrooms/%s" % date
+	PATH = "/Users/grantmcgovern/Desktop/HTMLwrite.txt"
+	############################################
+	print "Beginning a URL hit..."
+	print "URL: " + url + '\n'
 
-	url = "http://zsr.wfu.edu/studyrooms/2014/04/07"
 	IPfetch()
-	studyBug(url)
-	htmlFetch(url)
+	# studyBug(url)
+	htmlFetch(url, PATH)
 
 
-def htmlFetch(url):
+def htmlFetch(url, PATH): #must also pass in the path to the writeOut() call
 	link = url;
 	page = urllib2.urlopen(url)
 	soup = BeautifulSoup(page.read())
-	availability = soup.fieldset.find(id="room-203a").get_text()
-	print availability
+	availability = soup.find(id="room-203a").findAll("li", {"class" : "zone even open day"})
+	if availability:
+		writeOut(availability, PATH)
+	elif not availability:
+		print "Study Room 203A Completely Booked"
 
 def IPfetch():
 	print "Hostname is: " + socket.gethostname()
@@ -43,7 +53,16 @@ def studyBug(url):
 	assert "ZSR" in driver.title
 	elem = driver.find_element_by_id("room-203a")
 
+def writeOut(availability, PATH):
+	with open(PATH, "wb") as f:
+		print availability
+		string = str(availability)
+		f.write(string)
 
+
+def getDate():
+	date = time.strftime("%Y/%m/%d")
+	return date
 
 if __name__ == "__main__":
 	main()
