5297bb1dfd423254b92fa09541a0f431995c6674
Grant McGovern <grant@cobrain.com>
2014-04-08 14:32:10 -0400

    Added Class to Holder User Credentials

---
+++ Credentials.py
@@ -0,0 +1,21 @@
+#### Header file that contains Student's usernames and passwords
+
+import datetime 
+import sys 
+
+
+class User:
+
+	global_count = 0 # keeps track of a global count of how many Keys have been used 
+
+	def __init__(self, username, key):
+		self.username = username
+		self.key = key
+		User.global_count += 1
+
+	def showGlobalCount(self):
+		print "Number of Keys: %d" % global_count
+
+	def showKeys(self):
+		print "username: " + self.username
+		print "key: " + self.key + '\n'
--- Main.py
+++ Main.py
@@ -11,6 +11,7 @@ import socket
 import struct 
 import fcntl 
 import time
+from Credentials import * #stores information regarding usernames and passwords 
 from bs4 import BeautifulSoup
 from selenium import webdriver 
 from selenium.webdriver.common.keys import Keys 
@@ -20,11 +21,23 @@ def main():
 
 	date = getDate() # must make a call to get date and pass into URL to update with day 
 	############################################
+	### DO NOT CHANGE ###:
 	url = "http://zsr.wfu.edu/studyrooms/%s" % date
 	PATH = "/Users/grantmcgovern/Desktop/HTMLwrite.txt"
 	############################################
 	print "Beginning a URL hit..."
 	print "URL: " + url + '\n'
+
+	############################################
+	### Defines a Test User : Contains username/password
+	user1 = User("mcgoga12", "password")
+	user2 = User("shellb12", "password1")
+	user1.showKeys()
+	user2.showKeys()
+
+	print "Total User Count: %d \n" % User.global_count 
+
+	############################################
 
 	IPfetch()
 	# studyBug(url)
@@ -35,7 +48,7 @@ def htmlFetch(url, PATH): #must also pass in the path to the writeOut() call
 	link = url;
 	page = urllib2.urlopen(url)
 	soup = BeautifulSoup(page.read())
-	availability = soup.find(id="room-203a").findAll("li", {"class" : "zone even open day"})
+	availability = soup.find(id="room-203a").findAll("li", {"class" : "zone even open day"}) #finds if a study room is open or not 
 	if availability:
 		writeOut(availability, PATH)
 	elif not availability:
@@ -46,7 +59,10 @@ def IPfetch():
 	print "IP Address is: " + socket.gethostbyname(socket.gethostname())
 	print '\n'
 
+	### This function will eventually be used to change the IP address every time it's run
+
 def studyBug(url):
+	print "Crawling Site..."
 	print "Beginning a URL hit..."
 	driver = webdriver.Firefox()
 	driver.get(url)
