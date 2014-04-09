3d3ae4a3c30b9b0650714c9e49f480c76b7ffab2
Grant McGovern <grant@cobrain.com>
2014-04-09 09:05:50 -0400

    Updated with comments

--- Main.py
+++ Main.py
@@ -1,18 +1,18 @@
 # StudyBug 
 # Written and Bred by Grant McGovern <-- Written in the Python Language -->
 
-import re 
+import re # library that allows us to work wit Regular Expressions (Regex) --> will be used later to parse 
 import datetime 
 import sys 
 import os # allows us to interact with paths and open the file in a relative location 
-import urllib
-import urllib2
-import socket
+import urllib # library to work with URLs (helps go out to make the URL hit)
+import urllib2 # library to work with URLs (helps go out to make the URL hit)
+import socket # used to get IP address
 import struct 
 import fcntl 
 import time
 from Credentials import * #stores information regarding usernames and passwords 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup #imports HTML BeautifulSoup only instead of XML
 from selenium import webdriver 
 from selenium.webdriver.common.keys import Keys 
 
@@ -32,30 +32,34 @@ def main():
 	############################################
 	### Defines a Test User : Contains username/password
 	
-	readIn()
+	readIn() # function that reads in data from text file, current a dummy function as users as hard-coded below:
 
 	user1 = User("mcgoga12", "password")
 	user2 = User("shellb12", "password1")
-#	user1.showKeys()
-#	user2.showKeys()
+	user1.showKeys() # shows the username and password for user1
+	user2.showKeys() # shows the username and password for user2
 
-	print "Total User Count: %d \n" % User.global_count 
+	print "Total User Count: %d \n" % User.global_count # displays the amount of current users 
 
 	############################################
 
-	IPfetch()
-	studyBug(url)
-	htmlFetch(url, PATH)
+	IPfetch() # function to return IP address
+	studyBug(url) # function that kicks off the Web-Interactivity using Selenium 
+	htmlFetch(url, PATH) # grabs the HTML to see if rooms are available using BeautifulSoup
 
 
 def htmlFetch(url, PATH): #must also pass in the path to the writeOut() call
 	link = url;
-	page = urllib2.urlopen(url)
-	soup = BeautifulSoup(page.read())
+	page = urllib2.urlopen(url) # makes the URL hit
+	soup = BeautifulSoup(page.read()) # reads in the HTML data from the page 
 	print "What study room would you like to book? "
-	studyroom = "room-" + raw_input('> ')
+	studyroom = "room-" + raw_input('> ') # will be taken out, left in right now to work with specific study rooms 
 
-	availability_even_day = soup.find(id=studyroom).findAll("li", {"class" : "zone even open day"}) #finds if a study room is open or not 
+### zsr.wfu.edu/studyrooms is staggered in the following way:
+	# availability is marked by the 4 following options.
+	# each of these options stores the potential for being open or not
+
+	availability_even_day = soup.find(id=studyroom).findAll("li", {"class" : "zone even open day"}) 
 	availability_even_night = soup.find(id=studyroom).findAll("li", {"class" : "zone even open night"})
 	availability_odd_day = soup.find(id=studyroom).findAll("li", {"class" : "zone even odd day"})
 	availability_odd_night = soup.find(id=studyroom).findAll("li", {"class" : "zone even odd night"})
@@ -66,22 +70,25 @@ def htmlFetch(url, PATH): #must also pass in the path to the writeOut() call
 	print availability_odd_day
 	print availability_odd_night
 
+#########################################################
+
+	# if there exists ANY study room that is open, proceed to check individually 
 	if availability_even_day or availability_odd_day or availability_even_night or availability_odd_night:
-		if availability_even_day:
+		if availability_even_day: # if availability_even_day is the room open, then say so using writeOut()
 			writeOut(availability_even_day, PATH)
-		elif availability_odd_day:
+		elif availability_odd_day: # if availability_odd_day is the room open, then say so using writeOut()
 			writeOut(availability_odd_day)
-		elif availability_even_night:
+		elif availability_even_night: # if availability_even_night is the room open, then say so using writeOut()
 			writeOut(availability_even_night)
 		elif availability_odd_night:
-			writeOut(availability_odd_night)
+			writeOut(availability_odd_night) # if availability_odd_night is the room open, then say so using writeOut()
 		print "Study Room %s Available" % studyroom
-	else:
+	else: # otherwise if no Study Room is available, then print out that the specific study room is completely booked 
 		print "Study Room %s Completely Booked" % studyroom
 
 def IPfetch():
-	print "Hostname is: " + socket.gethostname()
-	print "IP Address is: " + socket.gethostbyname(socket.gethostname())
+	print "Hostname is: " + socket.gethostname() # returns the hostname 
+	print "IP Address is: " + socket.gethostbyname(socket.gethostname()) # returns your current IP address
 	print '\n'
 
 	### This function will eventually be used to change the IP address every time it's run
@@ -89,22 +96,23 @@ def IPfetch():
 def studyBug(url):
 	print "Crawling Site..."
 	print "Beginning a URL hit..."
+	# this opens up Chrome
 	driver = webdriver.Chrome(executable_path="/Users/grantmcgovern/Dropbox/Developer/Projects/StudyBug/chromedriver")
+	# this 	1
 	driver.get(url)
-	assert "ZSR" in driver.title
-	elem = driver.find_element_by_id("room-203a")
+	assert "ZSR" in driver.title # this assures that the word ZSR is in the webpage title (simply checks to make sure we'lre in the right spot)
+	elem = driver.find_element_by_id("room-203a") #looks for the HTML element for the specific study room
 
-def writeOut(availability, PATH):
+def writeOut(availability, PATH): # function used to write out to text file 
 	with open(PATH, "wb") as f:
 		print availability
 		string = str(availability)
 		f.write(string)
 
 def readIn():
-	
 	with open("credentials.txt", "r") as infile:
     		content = [line.rstrip().split(",") for line in infile]
-    		usernames, passwords = zip(*content)
+    		usernames, passwords = zip(*content) # nifty zip function used to split usernames and passwors into two seperate lists 
     		print usernames, passwords
 	for username in usernames:
 		print "username: %s" % username
