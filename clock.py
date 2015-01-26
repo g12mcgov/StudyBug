155874252d8547ab4b7b2a63d5123c6549616a3d
Grant McGovern <grant@cobrain.com>
2015-01-26 17:36:02 -0500

    Added clock.py scheduler

--- clock.py
+++ clock.py
@@ -20,9 +20,8 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-#@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
-#@sched.scheduled_job('interval', minutes=1)
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=22, minute=33)
+# Executes every night at 5:00am UTC time | 12:00am (midnight) Winston-Salem, NC time
+@sched.scheduled_job('cron', hour=5)
 def scheduled_job():
 	main()
     
