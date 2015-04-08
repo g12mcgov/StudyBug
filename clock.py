c5350c49ea8bb30f2740c1148f60505a5edfa78d
Grant McGovern <grant@cobrain.com>
2015-04-08 16:52:39 -0400

    Pusing another try 2

--- clock.py
+++ clock.py
@@ -24,9 +24,12 @@ from main import main
 sched = BlockingScheduler()
 
 # Executes every night at 5:00am UTC time | 12:00am (midnight) Winston-Salem, NC time
-@sched.scheduled_job('cron', hour=8, minute=52, misfire_grace_time=20)
-def scheduled_job():
-	main()
+# @sched.scheduled_job('cron', hour=8, minute=52, misfire_grace_time=20)
+# def scheduled_job():
+# 	main()
+@sched.scheduled_job('interval', minutes=1)
+def job():
+    print 'This job is run every minute.'
     
 sched.start()
 
