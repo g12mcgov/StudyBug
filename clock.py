47454dd057ed8c48f99f0d9bc207b6490591de7b
Grant McGovern <grant@cobrain.com>
2015-04-08 16:47:23 -0400

    Pusing another try 1

--- clock.py
+++ clock.py
@@ -24,7 +24,7 @@ from main import main
 sched = BlockingScheduler()
 
 # Executes every night at 5:00am UTC time | 12:00am (midnight) Winston-Salem, NC time
-@sched.scheduled_job('cron', hour=8, minute=45, misfire_grace_time=20)
+@sched.scheduled_job('cron', hour=8, minute=49, misfire_grace_time=20)
 def scheduled_job():
 	main()
     
