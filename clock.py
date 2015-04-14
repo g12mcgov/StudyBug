d9525d9b8766985f0d0f38fee87d3c53c73b2959
Grant McGovern <grant@cobrain.com>
2015-04-14 13:22:20 -0400

    Adding new times

--- clock.py
+++ clock.py
@@ -18,23 +18,19 @@ logging.basicConfig()
 
 sys.path.append('main')
 
-from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.blocking import BlockingScheduler
 from main import main
 
 #sched = BlockingScheduler()
 sched = BackgroundScheduler()
 
 # Executes every night at 5:00am UTC time | 12:00am (midnight) Winston-Salem, NC time
-@sched.scheduled_job('cron', hour=9, minute=23, misfire_grace_time=20)
+@sched.scheduled_job('cron', hour=9, minute=20, misfire_grace_time=20)
 def job():
 	print "In here"
 	main()
     
-# sched.add_job(job, 'cron', hour=9, minute=17, misfire_grace_time=20)
 sched.start()
 
-# DEBUG 
-# if __name__ == '__main__':
-#   pass 
 while True:
 	pass
